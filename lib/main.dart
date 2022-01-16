2500f4ba44f81a851d9c575caf6f2ee56cba1ad8
Aydcoder <aydcoder@gmail.com>
2022-01-16 16:42:58 +0100

    User panel Creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cswages/auth/splash.dart';
+import 'package:cswages/auth/user/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Splash(),
+      home: const Home(),
     );
   }
 }
